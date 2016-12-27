a634c31a0bf6a9bd3cc10242d3a263f39045259c
Tobias Ernst <info@tobias-ernst.ch>
2016-12-27 17:53:27 +0100

    abs syn

--- compiler/src/main/java/absSyn/FactorAbsSyn.java
+++ compiler/src/main/java/absSyn/FactorAbsSyn.java
@@ -1,34 +1,20 @@
 package absSyn;
-import conSyn.IConcSyn;
-import scanner.datatypes.Terminal;
-import scanner.token.IToken;
-import java.util.List;
+
+import scanner.errors.ContextError;
+
 /**
  * Created by ylaub on 26.12.2016.
  */
-public class FactorAbsSyn extends AbstractAbsSyn implements IAbsSyn{
-    private final List<IAbsSyn> OptionalIdentConcSyn;
-    private final List<IAbsSyn> MonadictOperatorConcSyn;
-    private final List<IAbsSyn> FactorConcSyn;
-    private final List<IAbsSyn> ExpressionConcSyn;
-    private final List<IAbsSyn> RepeatingOptionalExpressionsConcSyn;
-    private final List<IAbsSyn> ComplImagConcSyn;
-    private final List<IAbsSyn> ComplRealConcSyn;
+public class FactorAbsSyn extends AbstractAbsSyn implements IAbsSyn {
+    private IAbsSyn subType;
 
+    public FactorAbsSyn(IAbsSyn subType) {
 
-    public FactorAbsSyn(IToken t, List<IAbsSyn> OptionalIdentConcSyn, List<IAbsSyn> MonadictOperatorConcSyn, List<IAbsSyn> FactorConcSyn, List<IAbsSyn> ExpressionConcSyn, List<IAbsSyn> RepeatingOptionalExpressionsConcSyn, List<IAbsSyn> ComplImagConcSyn, List<IAbsSyn> ComplRealConcSyn) {
-        super(t);
-        this.OptionalIdentConcSyn = OptionalIdentConcSyn;
-        this.MonadictOperatorConcSyn = MonadictOperatorConcSyn;
-        this.FactorConcSyn = FactorConcSyn;
-        this.ExpressionConcSyn = ExpressionConcSyn;
-        this.RepeatingOptionalExpressionsConcSyn = RepeatingOptionalExpressionsConcSyn;
-        this.ComplImagConcSyn = ComplImagConcSyn;
-        this.ComplRealConcSyn = ComplRealConcSyn;
+        this.subType = subType;
     }
 
     @Override
-    public void check() {
-        //TODO: Implement Scope Check and Type Check
+    public void check() throws ContextError {
+        this.subType.check();
     }
 }
--- compiler/src/main/java/conSyn/FactorConcSyn.java
+++ compiler/src/main/java/conSyn/FactorConcSyn.java
@@ -1,65 +1,80 @@
 package conSyn;
 
 import absSyn.FactorAbsSyn;
-import absSyn.ProgramParameterListAbsSyn;
+import scanner.datatypes.Terminal;
 import scanner.errors.ContextError;
+import scanner.errors.GrammarError;
 import scanner.token.IToken;
 import scanner.tokenList.ITokenList;
-import java.util.List;
-import absSyn.IAbsSyn;
-import scanner.errors.GrammarError;
-import scanner.datatypes.Terminal;
 
 /**
  * Created by tobi on 17.12.16.
  */
 public class FactorConcSyn extends AbstractConcSyn implements IConcSyn {
+    private OptionalIdentConcSyn optionalIdentConcSyn;
+    private MonadictOperatorConcSyn monadictOperatorConcSyn;
+    private FactorConcSyn factorConcSyn;
+    private ExpressionConcSyn expressionConcSyn;
+    private RepeatingOptionalExpressionsConcSyn repeatingOptionalExpressionsConcSyn;
+    private ComplImagConcSyn complImagConcSyn;
+    private ComplRealConcSyn complRealConcSyn;
+    private IToken token;
+    private IConcSyn subType;
+
     public FactorConcSyn(ITokenList tokenList, int i) {
         super(tokenList, i);
     }
 
-    private IToken token;
     @Override
     public FactorAbsSyn toAbsSyn() throws ContextError {
-        //Für jedes Nichtterminalsymbol (unten mit ParseNext deklariert) wird eine Liste mit den dazugehörigen Elementen dem Abstrakten Syntaxbaum übergeben.
-        List<IAbsSyn> OptionalIdentConcSyn = super.getListByType(OptionalIdentConcSyn.class);
-        List<IAbsSyn> MonadictOperatorConcSyn = super.getListByType(MonadictOperatorConcSyn.class);
-        List<IAbsSyn> FactorConcSyn = super.getListByType(FactorConcSyn.class);
-        List<IAbsSyn> ExpressionConcSyn = super.getListByType(ExpressionConcSyn.class);
-        List<IAbsSyn> RepeatingOptionalExpressionsConcSyn = super.getListByType(RepeatingOptionalExpressionsConcSyn.class);
-        List<IAbsSyn> ComplImagConcSyn = super.getListByType(ComplImagConcSyn.class);
-        List<IAbsSyn> ComplRealConcSyn = super.getListByType(ComplRealConcSyn.class);
-
-        return new FactorAbsSyn(token, OptionalIdentConcSyn, MonadictOperatorConcSyn, FactorConcSyn, ExpressionConcSyn, RepeatingOptionalExpressionsConcSyn, ComplImagConcSyn, ComplRealConcSyn);
+        return new FactorAbsSyn(subType.toAbsSyn());
     }
-
 
     @Override
     public void parse() throws GrammarError {
         if (getTokenList().getCurrent().getTerminal() == Terminal.IMAGINARY_PART) {
+//            TODO: subType = new FactorImaginaryPartConcSyn();
             consume();
         } else if (getTokenList().getCurrent().getTerminal() == Terminal.LITERAL) {
+            // TODO: this.getTokenList().getCurrent().check();
+            // TODO: subType = new FactorLiteralConcSyn();
             consume();
         } else if (getTokenList().getCurrent().getTerminal() == Terminal.IDENT) {
+            // TODO: subType = new FactorIdentConcSyn();
             consume();
-            parseNext(new OptionalIdentConcSyn(getTokenList(), getCounter()));
+            optionalIdentConcSyn = new OptionalIdentConcSyn(getTokenList(), getCounter());
+            parseNext(optionalIdentConcSyn);
         } else if (getTokenList().getCurrent().getTerminal() == Terminal.ADDOPR
                 || getTokenList().getCurrent().getTerminal() == Terminal.NOT) {
-            parseNext(new MonadictOperatorConcSyn(getTokenList(), getCounter()));
-            parseNext(new FactorConcSyn(getTokenList(), getCounter()));
+            // TODO: subType = new FactorMoniadicConcSyn();
+            monadictOperatorConcSyn = new MonadictOperatorConcSyn(getTokenList(), getCounter());
+            parseNext(monadictOperatorConcSyn);
+
+            factorConcSyn = new FactorConcSyn(getTokenList(), getCounter());
+            parseNext(factorConcSyn);
         } else if (getTokenList().getCurrent().getTerminal() == Terminal.LPAREN) {
+            // TODO: subType = new FactorExpressionConcSyn();
             consume();
-            parseNext(new ExpressionConcSyn(getTokenList(), getCounter()));
-            parseNext(new RepeatingOptionalExpressionsConcSyn(getTokenList(), getCounter()));
+
+            expressionConcSyn = new ExpressionConcSyn(getTokenList(), getCounter());
+            parseNext(expressionConcSyn);
+
+            repeatingOptionalExpressionsConcSyn = new RepeatingOptionalExpressionsConcSyn(getTokenList(), getCounter())
+            parseNext(repeatingOptionalExpressionsConcSyn);
+
             if (getTokenList().getCurrent().getTerminal() == Terminal.RPAREN) {
                 consume();
             } else {
                 throwGrammarError();
             }
         } else if (getTokenList().getCurrent().getTerminal() == Terminal.IMAG) {
-            parseNext(new ComplImagConcSyn(getTokenList(), getCounter()));
+            // TODO: subType = new FactorImagConcSyn();
+            complImagConcSyn = new ComplImagConcSyn(getTokenList(), getCounter());
+            parseNext(complImagConcSyn);
         } else if (getTokenList().getCurrent().getTerminal() == Terminal.REAL) {
-            parseNext(new ComplRealConcSyn(getTokenList(), getCounter()));
+            // TODO: subType = new FactorRealConcSyn();
+            complRealConcSyn = new ComplRealConcSyn(getTokenList(), getCounter());
+            parseNext(complRealConcSyn);
         } else {
             throwGrammarError();
         }
