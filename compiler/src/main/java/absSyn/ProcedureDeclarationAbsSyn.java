7f2ac3498f63b178ea669597d83b59fa5ef4ddd5
yves lauber <ylauber91@gmail.com>
2016-12-28 16:08:05 +0100

    more ConcSyn rewrites

--- compiler/src/main/java/absSyn/ProcedureDeclarationAbsSyn.java
+++ compiler/src/main/java/absSyn/ProcedureDeclarationAbsSyn.java
@@ -2,18 +2,21 @@ package absSyn;
 
 import scanner.errors.ContextError;
 import virtualmachineFS2015.ICodeArray;
+import scanner.token.Ident;
 
 /**
  * Created by ylaub on 26.12.2016.
  */
 public class ProcedureDeclarationAbsSyn extends AbstractAbsSyn implements IAbsSyn {
 
+    private Ident ident;
     private final ParameterListAbsSyn parameterListAbsSyn;
     private final OptionalGlobalImportsAbsSyn optionalGlobalImportsAbsSyn;
     private final OptionalLocalStorageDeclarationsAbsSyn optionalLocalStorageDeclarationsAbsSyn;
     private final BlockCmdAbsSyn blockCmdAbsSyn;
 
-    public ProcedureDeclarationAbsSyn(ParameterListAbsSyn parameterListAbsSyn, OptionalGlobalImportsAbsSyn optionalGlobalImportsAbsSyn, OptionalLocalStorageDeclarationsAbsSyn optionalLocalStorageDeclarationsAbsSyn, BlockCmdAbsSyn blockCmdAbsSyn) {
+    public ProcedureDeclarationAbsSyn(Ident ident, ParameterListAbsSyn parameterListAbsSyn, OptionalGlobalImportsAbsSyn optionalGlobalImportsAbsSyn, OptionalLocalStorageDeclarationsAbsSyn optionalLocalStorageDeclarationsAbsSyn, BlockCmdAbsSyn blockCmdAbsSyn) {
+        this.ident = ident;
 
         this.parameterListAbsSyn = parameterListAbsSyn;
         this.optionalGlobalImportsAbsSyn = optionalGlobalImportsAbsSyn;
@@ -23,6 +26,7 @@ public class ProcedureDeclarationAbsSyn extends AbstractAbsSyn implements IAbsSy
 
     @Override
     public void check() throws ContextError {
+        ident.check();
         this.parameterListAbsSyn.check();
         this.optionalGlobalImportsAbsSyn.check();
         this.optionalLocalStorageDeclarationsAbsSyn.check();
