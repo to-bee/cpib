f632298685aa5df8f8c2a9e44a6a58400ac1d696
Malin Ekdahl <malin.ekdahl@students.fhnw.ch>
2016-12-28 12:32:41 +0100

    add vm

---
+++ compiler/src/main/java/virtualmachineFS2015/ICodeArray.java
@@ -0,0 +1,20 @@
+// Virtual Machine Java 2015, V01
+// Edgar F.A. Lederer, FHNW and Uni Basel, 2015
+
+package virtualmachineFS2015;
+import virtualmachineFS2015.IInstructions.*;
+
+public interface ICodeArray {
+    // for the COMPILER:
+    // a CodeTooSmallError indicates that the code is too small
+    // to hold the complete program
+    class CodeTooSmallError extends Exception {}
+    void put(int loc, IInstr instr) throws CodeTooSmallError;
+    void resize();
+    String toString();
+
+    // for the VM:
+    int getSize();
+    IInstr get(int loc);
+    void fromString();
+}
---
+++ compiler/src/main/java/virtualmachineFS2015/InputUtility.java
@@ -0,0 +1,49 @@
+// Virtual Machine Java 2015, V01
+// Edgar F.A. Lederer, FHNW and Uni Basel, 2015
+
+package virtualmachineFS2015;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import virtualmachineFS2015.IVirtualMachine.*;
+
+class InputUtility {
+
+    private static BufferedReader reader=
+            new BufferedReader(new InputStreamReader(System.in));
+
+    public static boolean readBool() throws ExecutionError {
+        String s;
+        try {
+            s= reader.readLine();
+        } catch (IOException e) {
+            throw new ExecutionError("Input failed.");
+        }
+        if (s.equals("false")) {
+            return false;
+        }
+        else
+        if (s.equals("true")) {
+            return true;
+        }
+        else {
+            throw new ExecutionError("Not a boolean.");
+        }
+    }
+
+    public static int readInt() throws ExecutionError {
+        String s;
+        try {
+            s= reader.readLine();
+        } catch (IOException e) {
+            throw new ExecutionError("Input failed.");
+        }
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            throw new ExecutionError("Not an integer.");
+        }
+    }
+}
